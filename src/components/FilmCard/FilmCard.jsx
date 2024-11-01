1168157c6677fdb3c594a26a55124c254fe21fcf
Helin Biçen <helinbicen@outlook.com>
2024-11-01 12:10:40 +0300

    Entegrate backend, fix styles, add api helper

--- src/components/FilmCard/FilmCard.jsx
+++ src/components/FilmCard/FilmCard.jsx
@@ -2,47 +2,58 @@ import React from "react";
 import style from "./FilmCard.module.css";
 import { BsClockFill } from "react-icons/bs";
 
-
 function FilmCard({ film }) {
   return (
     <div
-      style={{ backgroundColor: `#${film.average_colour_hex}` }}
+      style={{ backgroundColor: `#${film.filmColor}` }}
       className={style.cardContainer}
     >
       <div
-        className={style.filmCover}
-        style={{ background: `url("${film.still_url}")` }}
+        className={style.filmImg}
+        style={{ background: `url("${film.filmImg}")` }}
       />
 
-      <div className={style.filmInfo}>
-        <div className={style.titleContainer}>
-          <p className={style.title}>{film.original_title}</p>
-         
-          <p className={style.year}>({film.year})</p>
-        </div>
-        <p className={style.director}>
-          {film.directors.map((director) => `directed by ${director.name}`)}
-        </p>
-        <div className={style.durationContainer}>
-        <BsClockFill />
-        <p className={style.duration}>{film.duration} minutes</p>
-       
-        </div>
-        <ul className={style.genres}>
-          {film.genres.map((genre) => (
-            <li className={style.genre}>● {genre}</li>
-          ))}
-        </ul>
-        <p className={style.averageRating}>
-          {film.average_rating_out_of_ten} / 10
-        </p>
-      </div>
+      <div className={style.content}>
+        <div className={style.filmInfo}>
+          <div className={style.titleContainer}>
+            <p className={style.title}>{film.filmName}</p>
 
-      <div className={style.descriptionContainer}>
-        <p className={style.shortSynopsis}>{film.short_synopsis}</p>
-        <a className={style.webUrl} href={film.web_url}>
-          <button className={style.urlButton}>Click to Open on Mubi</button>
-        </a>
+            <p className={style.year}>({film.filmYear})</p>
+          </div>
+          <p className={style.director}>directed by {film.filmDirector}</p>
+          <div className={style.durationContainer}>
+            <BsClockFill />
+            <p className={style.duration}>{film.fiLmDuration} minutes</p>
+          </div>
+          <ul className={style.genres}>
+            {film.filmGenre.map((genre) => (
+              <li className={style.genre}>● {genre}</li>
+            ))}
+          </ul>
+          <div className={style.ratingContainer}>
+            <p>
+              <span className={style.ratingType}>Mubi Rating: </span>
+              <span className={style.mubiRating}> {film.mubiRating} / 10 </span>
+            </p>
+            <p>
+              <span className={style.ratingType}>Imdb Rating: </span>
+              <span className={style.imdbRating}>{film.imdbRating} / 10</span>
+            </p>
+            <p>
+              <span className={style.ratingType}>Average Rating: </span>
+              <span className={style.averageRating}>
+                {film.averageRating} / 10
+              </span>
+            </p>
+          </div>
+        </div>
+
+        <div className={style.descriptionContainer}>
+          <p className={style.shortSynopsis}>{film.filmDescription}</p>
+          <a className={style.webUrl} href={film.filmWebUrl}>
+            <button className={style.urlButton}>Click to Open on Mubi</button>
+          </a>
+        </div>
       </div>
     </div>
   );
